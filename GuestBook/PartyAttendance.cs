6113cdf20bd02010cc1cb6410551a29b3962ca16
iloozealot <iloozealot@gmail.com>
2023-11-17 18:58:26 -0600

    Add project files.

---
+++ GuestBook/PartyAttendance.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using static System.Console;
+
+namespace GuestBook
+{
+    public static class PartyAttendance
+    {
+        static List<string> guestBook = new();
+        static List<int> partyNumbers = new();
+        static string partyName;
+        static int partyNumber;
+        static bool anotherGroup = false;
+        static int result = 0;
+        public static void GetPartyNames()
+        {
+            Write("Please enter name of party: ");
+            partyName = ReadLine();
+            guestBook.Add(partyName);
+        }
+        public static void GetPartyNumbers()
+        {
+            Write("Please enter number in party: ");
+            partyNumber = Convert.ToInt32(ReadLine());
+            partyNumbers.Add(partyNumber);
+        }
+        public static void PartyNames()
+        {
+            foreach (string partyName in guestBook)
+            {
+                WriteLine($"Parties in attendance: {partyName}");
+            }
+        }
+        public static void GuestNumbers()
+        {
+            foreach (int partyNumber in partyNumbers)
+            {
+                result += partyNumber;
+            }
+
+            WriteLine($"The number of people in attendance is {result}");
+        }
+    }
+}
---
+++ GuestBook/Program.cs
@@ -0,0 +1,51 @@
+﻿
+//Console application?
+//Ask the user for their name and how many are in their party?
+//Track how many people are at the party?
+//Print Guest List and Total number of guests?
+
+using GuestBook;
+using static System.Console;
+
+bool isRunning = false;
+
+WriteLine("Welcome to the party!");
+WriteLine("-------------------------------------------");
+
+do
+{
+    Console.WriteLine();
+
+    PartyAttendance.GetPartyNames();
+
+    Console.WriteLine();
+
+    PartyAttendance.GetPartyNumbers();
+
+    Console.WriteLine();
+
+    Write("Is there another party y/n: ");
+
+
+    if (ReadLine() == "y")
+    {
+        isRunning = true;
+    }
+    else
+    {
+        isRunning = false;
+    }
+
+} while (isRunning == true);
+
+WriteLine();
+
+PartyAttendance.PartyNames();
+
+WriteLine();
+
+PartyAttendance.GuestNumbers();
+
+WriteLine();
+
+WriteLine("Thank you for coming!");
